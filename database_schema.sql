7e2d0b200e54b74314d9d05aff9afd4a867f4d57
Hodgey <c5059061@hallam.shu.ac.uk>
2026-04-17 10:01:25 +0100

    Added data
    Example data added to test project.

--- database_schema.sql
+++ database_schema.sql
@@ -33,3 +33,43 @@ CREATE TABLE Booking (
     FOREIGN KEY (UserID) REFERENCES Account(UserID),
     FOREIGN KEY (BookID) REFERENCES Inventory(BookID)
 );
+
+
+
+
+
+--ACCOUNT DATA 
+INSERT INTO Account (UserID, FirstName, LastName, Password, Email, Role) VALUES
+(1, 'John', 'Smith', 'pass1', 'JohnSmith@gmail.com', 'User'),
+(2, 'Jane', 'Smith', 'pass2', 'JaneSmith@gmail.com', 'Admin'),
+(3, 'Alex', 'Jones', 'pass3', 'AlexJones@gmail.com', 'User');
+
+--BOOK DESCRIPTIONS 
+INSERT INTO BookDescribed (ISBN, Title, Author, Published, Genre, Description) VALUES
+('ISBN-01', 'The Great Gatsby', 'F. Scott Fitzgerald', 1925, 'Classic', 'A story of the Jazz Age.'),
+('ISBN-02', '1984', 'George Orwell', 1949, 'Dystopian', 'Big Brother is watching.'),
+('ISBN-03', 'The Hobbit', 'J.R.R. Tolkien', 1937, 'Fantasy', 'A hobbits journey.'),
+('ISBN-04', 'Brave New World', 'Aldous Huxley', 1932, 'Dystopian', 'A programmed society.'),
+('ISBN-05', 'Hamlet', 'William Shakespeare', 1603, 'Tragedy', 'The Prince of Denmark.'),
+('ISBN-06', 'Moby Dick', 'Herman Melville', 1851, 'Adventure', 'The hunt for the white whale.');
+
+--INVENTORY
+INSERT INTO Inventory (BookID, ISBN, Status) VALUES
+(101, 'ISBN-001', 'Checked Out'),
+(102, 'ISBN-002', 'Checked In'),
+(103, 'ISBN-003', 'Checked Out'),
+(104, 'ISBN-004', 'Checked Out'),
+(105, 'ISBN-005', 'Checked Out'),
+(106, 'ISBN-006', 'Checked Out');
+
+--BOOKINGS
+INSERT INTO Booking (BookingID, UserID, BookID, TimeOut, TimeDueIn, Returned) VALUES
+
+(1001, 1, 101, UNIX_TIMESTAMP(), UNIX_TIMESTAMP() + 1209600, 0),
+
+(1002, 2, 102, 1735689600, 1736899200, 0), --Overdue
+(1003, 2, 103, UNIX_TIMESTAMP(), UNIX_TIMESTAMP() + 1209600, 0),
+
+(1004, 3, 104, 1735689600, 1736899200, 0), --Overdue
+(1005, 3, 105, UNIX_TIMESTAMP(), UNIX_TIMESTAMP() + 1209600, 0),
+(1006, 3, 106, UNIX_TIMESTAMP(), UNIX_TIMESTAMP() + 1209600, 0);
